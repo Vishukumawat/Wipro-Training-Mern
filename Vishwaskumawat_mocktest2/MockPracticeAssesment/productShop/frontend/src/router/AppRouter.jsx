a0be56c492a7c14ff9ad5836f7edc4e2d10cc35b
Vishukumawat <rkumawat4151@gmail.com>
2025-11-22 00:41:45 +0530

    mock 2 update

--- Vishwaskumawat_mocktest2/MockPracticeAssesment/productShop/frontend/src/router/AppRouter.jsx
+++ Vishwaskumawat_mocktest2/MockPracticeAssesment/productShop/frontend/src/router/AppRouter.jsx
@@ -1,21 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "../App";
 import UserDetails from "../pages/UserDetails";
-import FormikLogin from "../pages/FormikLogin";  
+import FormikLogin from "../pages/FormikLogin";
 
-export default function AppRouter() {
+function AppRouter() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        
         <Route path="/" element={<App />} />
-
-        
-        <Route path="/users/:id" element={<UserDetails />} />
-
        
+        <Route path="/users/:id" element={<UserDetails />} />
         <Route path="/formik-login" element={<FormikLogin />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
+
+export default AppRouter;
